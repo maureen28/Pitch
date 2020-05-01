caa59bac912d713e7f8b4e582645a476a548922f
Maureen <murimimaureen69@gmail.com>
2020-05-02 00:32:36 +0300

    Create manager shell and add a dictionary

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from config import config_options
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection ='strong'
+login_manager.login_viewv= 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
--- app/models.py
+++ app/models.py
@@ -1,4 +1,22 @@
+from . import db
+
 class User(db.Model):
-     ''' user class to define user Objects
-    '''
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(50), unique = True)
+    email = db.Column(db.String(50), unique = True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    
+    def __repr__(self):
+        return f'User {self.username}'
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(50), unique = True)
+    email = db.Column(db.String(50), unique = True)
+    users = db.relationship('User', backref = 'role', lazy = "dynamic")
+    
+    def __repr__(self):
+        return f'User {self.username}'
    
