5c154272f1ab2755791771bc90e3a0ed0d5f3819
Maureen <murimimaureen69@gmail.com>
2020-05-01 22:11:36 +0300

    Add error page and alert danger class

--- test/test_user.py
+++ test/test_user.py
@@ -1,7 +1,15 @@
-import unittest
-from app.models import User
+# import unittest
+# from app.models import User
 
 # class TestUser(unittest.TestCase):
 #       '''
 #     Test Class to test the behaviour of the User class
 #     '''
+#     def setUp(self):
+#         '''
+#         Set up method that will run before every Test
+#         '''
+#         self.new_user = User()
+    
+#     def test_instance(self):
+#         self.assertTrue(isinstance(self.new_user, User))
