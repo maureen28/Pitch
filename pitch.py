42a8a335324c3ad6de6ce6326e2902d9838e7f05
Maureen <murimimaureen69@gmail.com>
2020-05-05 01:10:15 +0300

    Add class Pitch and user

--- pitch.py
+++ pitch.py
@@ -1,8 +1,33 @@
 from flask import Flask, render_template,flash,redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm,LoginForm
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY']= '754aeac211d43da4de04692f6d1ffd5a'
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:////site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(20),unique= True, nullable = False )
+    email = db.Column(db.String(120),unique= True, nullable = False )
+    image_file = db.Column(db.String(20) , nullable = False,default = 'default.jpg' )
+    password = db.Column(db.String(60) , nullable = False )
+    
+
+    def __repr__(self):
+        return f'User("{self.username}", "{self.email}","{self.image_file}")'
+
+class Pitch(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), nullable = False)
+    date_posted = db.Column(db.dateTime,nullable = False, default = datetime.utcnow)
+    content = db.Column(db.Text, nullable =False)
+ 
+       def __repr__(self):
+        return f'Pitch("{self.title}",,"{self.date_posted}")'
+
 
 @app.route('/')
 @app.route('/home')
@@ -22,9 +47,15 @@ def register():
         return redirect(url_for('home'))
     return render_template('register.html',title='Register', form =form)
     
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data =='murimimaureen8@gmail.com' and form.password.data == 'password':
+            flash(f'Successful login ','success')
+            return redirect(url_for('home'))
+        else: 
+            flash(f'Unsuccessful login.Please check username and password ','danger') 
     return render_template('login.html',title='Login', form =form)
     
 
