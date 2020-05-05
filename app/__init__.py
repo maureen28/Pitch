2755bcfaa928b30996fe93ede0534c10f9549ba9
Maureen <murimimaureen69@gmail.com>
2020-05-05 17:23:35 +0300

    import db to models

--- app/__init__.py
+++ app/__init__.py
@@ -1,32 +1,13 @@
 from flask import Flask, render_template,flash,redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from forms import RegistrationForm,LoginForm
+from models import RegistrationForm,LoginForm
 from datetime import datetime
+from .models import User,Post
 
 app = Flask(__name__)
 app.config['SECRET_KEY']= '754aeac211d43da4de04692f6d1ffd5a'
 app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:////site.db'
 db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(20),unique= True, nullable = False )
-    email = db.Column(db.String(120),unique= True, nullable = False )
-    image_file = db.Column(db.String(20) , nullable = False,default = 'default.jpg' )
-    password = db.Column(db.String(60) , nullable = False )
-    
-
-    def __repr__(self):
-        return f'User("{self.username}", "{self.email}","{self.image_file}")'
-
-class Pitch(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    title = db.Column(db.String(100), nullable = False)
-    date_posted = db.Column(db.dateTime,nullable = False, default = datetime.utcnow)
-    content = db.Column(db.Text, nullable =False)
- 
-       def __repr__(self):
-        return f'Pitch("{self.title}",,"{self.date_posted}")'
 
 
 @app.route('/')
@@ -60,4 +41,4 @@ def login():
     
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug= True)
