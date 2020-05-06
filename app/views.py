d4ceec933ba1d5a872f4e6f5105df41c9132a6a7
Maureen <murimimaureen69@gmail.com>
2020-05-06 11:23:46 +0300

    Add text primary, rounded circle and heading

--- app/views.py
+++ app/views.py
@@ -39,8 +39,8 @@ def login():
         user = User.query.filter_by(email= form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember = form.remember.data)
-            next_page = request.args.get()
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Unsuccessful login. Please check your email and password', 'danger')
     return render_template('login.html',title='Log In', form =form)
