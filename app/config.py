9ed9f10b0cf13632c422d124a2715e0a50b79eb9
Maureen <murimimaureen69@gmail.com>
2020-05-09 23:29:17 +0300

    updated database url

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = '8070dceedbac32d866d2ea3fb103117d'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    DATABASE_URL = 'sqlite:///site.db'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
